6986d842a8e7fe6c324ba9d3047fd61529a2c9d1
David Kent <davidkent@fastmail.com.au>
2016-08-04 20:07:48 +1000

    Add cmake config.

--- _build.py
+++ _build.py
@@ -1,16 +1,19 @@
+import os
+
 import cffi
 
 ffibuilder = cffi.FFI()
 
-header = 'include/my_model.h'
-frozen_py_code = 'frozen.py'
+src_path = os.path.dirname(__file__)
+header = os.path.join(src_path, 'include', 'my_model.h')
+frozen_py_code = os.path.join(src_path, 'frozen.py')
 
 with open(header) as fobj:
     ffibuilder.embedding_api(fobj.read())
 
-ffibuilder.set_source("my_model", "")
+ffibuilder.set_source("my_model", "", extra_link_args=['-Wl,-soname,my_model.so'])
 
 with open(frozen_py_code) as fobj:
     ffibuilder.embedding_init_code(fobj.read())
 
-ffibuilder.compile(target="plugin.*", verbose=True)
+ffibuilder.compile(verbose=True)
